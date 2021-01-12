2e1ceebae6bc81f38c0c779489c7489f61c0febf
wdong20 <wdong20@stuy.edu>
2021-01-12 11:51:27 -0500

    Created skeletons for helper methods

---
+++ Radix.java
@@ -0,0 +1,15 @@
+public class Radix {
+    // get nth digit of an int, where 0 is the ones column, 1 is tens, etc.
+    public static int nth(int n, int col) {
+        return -1;
+    }
+
+    // return the number of digits in n
+    public static int length(int n) {
+        return -1;
+    }
+
+    public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+
+    }
+}
