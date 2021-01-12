8c6f9bfe385f9ba461a908a2a18a509dba848a81
wdong20 <wdong20@stuy.edu>
2021-01-12 12:03:30 -0500

    Implemented nth() - Tested and working as intended

--- Radix.java
+++ Radix.java
@@ -1,7 +1,10 @@
+import java.lang.Math;
+
 public class Radix {
     // get nth digit of an int, where 0 is the ones column, 1 is tens, etc.
     public static int nth(int n, int col) {
-        return -1;
+        int place = (int)Math.pow(10, col);
+        return (n / place) % 10;
     }
 
     // return the number of digits in n
