89227607a5c45daeb390914aed94dcfbd8a0c101
wdong20 <wdong20@stuy.edu>
2021-01-13 23:24:13 -0500

    Added tests for nth() where the number is negative

--- Tester.java
+++ Tester.java
@@ -1,8 +1,12 @@
 public class Tester {
 	public static void main(String[] abc) {
+		System.out.println("Expects 5: "+Radix.nth(-12345, 0));
 		System.out.println("Expects 5: "+Radix.nth(12345, 0));
+		System.out.println("Expects 4: "+Radix.nth(-12345, 1));
 		System.out.println("Expects 4: "+Radix.nth(12345, 1));
+		System.out.println("Expects 2: "+Radix.nth(-12345, 3));
 		System.out.println("Expects 2: "+Radix.nth(12345, 3));
+		System.out.println("Expects 0: "+Radix.nth(-12345, 5));
 		System.out.println("Expects 0: "+Radix.nth(12345, 5));
 		
 		System.out.println("Expects 1: "+Radix.length(1));
