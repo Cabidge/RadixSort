a761a18484cada515c6082dd63024c9b709ef4aa
wdong20 <wdong20@stuy.edu>
2021-01-13 23:06:36 -0500

    Created and tested radixSort() - working as intended. Code is fairly messy, but I'm too tired to clean it up

--- Radix.java
+++ Radix.java
@@ -44,7 +44,48 @@ public class Radix {
     }
 
     public static void radixSort(SortableLinkedList data) {
+        if (data.size() <= 1) {
+            return;
+        }
 
+        // split lists
+        int maxLength = 0;
+        SortableLinkedList[] buckets = Radix.makeBuckets();
+        SortableLinkedList[] bucketsNeg = Radix.makeBuckets();
+        Integer current;
+        while (data.size() > 0) {
+            current = data.remove(0);
+            SortableLinkedList bucket;
+
+            maxLength = Math.max(maxLength, Radix.length(current));
+            if (current.compareTo(0) < 0) {
+                bucket = bucketsNeg[Radix.nth(current, 0) + 9];
+            } else {
+                bucket = buckets[Radix.nth(current, 0)];
+            }
+            bucket.add(current);
+        }
+
+        SortableLinkedList negativeData = new SortableLinkedList();
+        Radix.merge(data, buckets);
+        Radix.merge(negativeData, bucketsNeg);
+
+        for (int i = 1; i < maxLength; i++) {
+            while (data.size() > 0) {
+                current = data.remove(0);
+                buckets[Radix.nth(current, i)].add(current);
+            }
+            Radix.merge(data, buckets);
+
+            while (negativeData.size() > 0) {
+                current = negativeData.remove(0);
+                bucketsNeg[Radix.nth(current, i) + 9].add(current);
+            }
+            Radix.merge(negativeData, bucketsNeg);
+        }
+
+        negativeData.extend(data);
+        data.extend(negativeData);
     }
 
     private static SortableLinkedList[] makeBuckets() {
